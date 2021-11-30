8811657a9b2e1d5d981180d6b09654822efa8d3c
li-lam <leeleelam@gmail.com>
2021-11-30 12:11:53 -0700

    edited most of the about page

--- inst/About.R
+++ inst/About.R
@@ -10,28 +10,23 @@ function(){
 		  <br>
       <div style="max-width:800px; word-wrap:break-word;">
         <p style="font-size:100%;text-align:justify">
-          The BotanizeR Shiny application allows to test and improve your 
-          botanical knowledge for a defined set of species based on a defined 
-          set of images and ecological and morphological descriptions. 
+          The PollinatorR Shiny application allows citizens to test and improve
+          their polliantor identification skills based on their images.
           <br>
           <br>
-          In the Species tab, you can retrieve all deposited information for 
-          each species.
+          In the Species tab, you can practice identifying pollinators seen
+          in the Calgary area by choosing from the list and scrolling through
+          the images.
           <br>
           <br>
-          In the Quiz tab you can practice your species identification skills. 
-          The app will show you random species and you are asked to provide the 
-          species name (case insensitive). You may browse through a selection 
-          of pictures and enable additional hints based on species 
-          morphological and ecological descriptions. The app gives feedback on 
-          whether your attempt was correct or not and if not, how different the 
-          actual name is and whether the genus was correct or not. The quiz 
-          keeps track of the species shown and answered correctly and uses this 
-          information to show species that ave been answered correctly less 
+          In the Quiz tab you can practice your pollinator identification 
+          skills. The app will show you random species and you are asked to 
+          provide the pollinator name (case insensitive). You may browse 
+          through a selection of pictures. The quiz keeps track of the species 
+          shown and answered correctly and uses this information to show species 
+          that have been answered correctly less 
           frequently and species which have not been answered correctly more 
-          frequently (if enabled). You can download the species list after 
-          practicing and upload it again to make use of the saved numbers of 
-          successes and failures when practicing again.
+          frequently. 
           <br>',
              ifelse(setup, '<br>
           In the Setup tab you can choose which species to practice, either by 
@@ -49,41 +44,9 @@ function(){
           <br>', ''),
              '<br>
       </p>
-      <span style="color:#64645F;font-weight: bold;">This Example</span>
-		  <br>
-		  <br>
-      <div style="max-width:800px; word-wrap:break-word;">
-        <p style="font-size:100%;text-align:justify">',
-             instance_description,
-        '<br>
-         <br>
-      </p>
-      <span style="color:#64645F;font-weight: bold;">
-      General applicability</span>
-		  <br>
-		  <br>
-        <p style="font-size:100%;text-align:justify">
-          BotanizeR is highly flexible and easy to set up, allowing lecturers 
-          to define themselves the species list, picture base and species 
-          characteristics to show to their students. Technically, BotanizeR is 
-          an R-package which can be installed from 
-          <a href="https://github.com/patrickweigelt/BotanizeR" target=_blank>
-          GitHub</a>. Tutorials on how to setup BotanizeR are vailable 
-          <a href="https://patrickweigelt.github.io/BotanizeR/index.html" 
-          target=_blank>here</a>.
-          The quiz can be played within R or better RStudio or alternatively it 
-          can be made available via the package&rsquo;s Shiny app. The Shiny 
-          app can be launched locally after installing the package or on a 
-          Shiny server to make it accessible to a broader audience. When using 
-          it locally students could be provided with a species list with 
-          descriptions and an image folder for download which can then be 
-          loaded from within the Shiny app and used for practicing. When 
-          launching it on a Shiny server, BotanizeR can be accessed online via 
-          an URL and species lists, descriptions and pictures can be made 
-          available right away within the app like exemplified here.
-          <br>
-          <br>
-        </p>
+
+
+
       </div>   
       <hr width="800", align="left" style="height:0.5px;border:none;
       color:#A0A5A8;background-color:#A0A5A8;" />
@@ -94,17 +57,8 @@ function(){
          instance_credits,
 		     '</p>
 		  </div>  
-      <br>
-		  <span style="color:#64645F;font-weight: bold;">Authors</span>
-		  <div style="max-width:1000px; word-wrap:break-word;">
-		     <p style="text-align:justify">
-		        The BotanizeR R-package and Shiny app have been developed at 
-		        <a href="https://www.uni-goettingen.de/en/128741.html" 
-		        target=_blank>Biodiversity, Macroecology and Biogeography</a> 
-		         by <a href="https://www.uni-goettingen.de/en/157014.html" 
-		         target=_blank>Patrick Weigelt</a> & Pierre Denelle.
-		     </p>
-		  </div>  
+
+  
       <br>
 		  <span style="color:#64645F;font-weight: bold;">Citation</span>
 		  <div style="max-width:1000px; word-wrap:break-word;">
